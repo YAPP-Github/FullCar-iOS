ec3e0989f634db9271a09e3273062dd806c0f6a7
SunnnySong <tjsghktjswls@naver.com>
2023-12-17 23:42:24 +0900

    feat: AccountService 타입 생성 및 구현

---
+++ Targets/FullCarKit/Sources/Account/AccountService.swift
@@ -0,0 +1,57 @@
+//
+//  AccountService.swift
+//  FullCarKit
+//
+//  Created by Sunny on 12/17/23.
+//  Copyright © 2023 FullCar Corp. All rights reserved.
+//
+
+import Foundation
+import Dependencies
+
+public struct AccountService {
+
+    @Dependency(\.accountAPI) var api
+    @Dependency(\.tokenStorage) var tokenStorage
+
+    public var hasValidToken: Bool {
+        guard let credential = tokenStorage.loadToken() else { return false }
+        return credential.authTokenExpiration > Date()
+    }
+
+    public func login(accessToken: String) throws {
+        let response = api.login(accessToken: accessToken)
+
+        // 서버에서 받아온 account token 저장
+        switch response {
+        case .success(let token):
+            let credential = AccountCredential(
+                authToken: token,
+                authTokenExpiration: Date()
+            )
+            tokenStorage.save(token: credential)
+        case .failure(let error):
+            throw error
+        }
+    }
+
+    public func logout() {
+        api.logout()
+    }
+
+    public func leave() {
+        api.leave()
+        tokenStorage.deleteToken()
+    }
+}
+
+extension DependencyValues {
+    public var accountService: AccountService {
+        get { self[AccountService.self] }
+        set { self[AccountService.self] = newValue }
+    }
+}
+
+extension AccountService: DependencyKey {
+    public static var liveValue: AccountService = .init()
+}
