ec6009785d31990efe668ec14fbd27d944155998
SunnnySong <tjsghktjswls@naver.com>
2023-12-28 01:15:08 +0900

    feat: Color extension 생성 및 구현

---
+++ Targets/FullCarUI/Sources/Color/Color+FullCar.swift
@@ -0,0 +1,40 @@
+//
+//  Color+FullCar.swift
+//  FullCarUI
+//
+//  Created by Sunny on 12/28/23.
+//  Copyright © 2023 FullCar Corp. All rights reserved.
+//
+
+import SwiftUI
+
+// MARK: Warm Gray
+public extension Color {
+    static let warmGray1: Color = .init(uiColor: .init(hex: "D6D2CD"))
+    static let warmGray2: Color = .init(uiColor: .init(hex: "727272"))
+    static let warmGray3: Color = .init(uiColor: .init(hex: "393939"))
+    static let warmGray4: Color = .init(uiColor: .init(hex: "242424"))
+    static let day_black: Color = .init(uiColor: .init(hex: "000000"))
+}
+
+// MARK: Gray
+public extension Color {
+    static let white: Color = .init(uiColor: .init(hex: "FAFAFB"))
+    static let gray1: Color = .init(uiColor: .init(hex: "ECECEC"))
+    static let gray2: Color = .init(uiColor: .init(hex: "B1B8C0"))
+    static let gray3: Color = .init(uiColor: .init(hex: "C4C3C2"))
+    static let gray4: Color = .init(uiColor: .init(hex: "8B95A1"))
+    static let gray5: Color = .init(uiColor: .init(hex: "4E5968"))
+}
+
+// MARK: Main Color
+public extension Color {
+    static let fullCar_primary: Color = .init(uiColor: .init(hex: "6C7AF1"))
+    static let fullCar_secondary: Color = .init(uiColor: .init(hex: "E1E4FE"))
+}
+
+// MARK: System Color
+public extension Color {
+    static let warning: Color = .init(uiColor: .init(hex: "FF4B4B"))
+    static let success: Color = .init(uiColor: .init(hex: "00CF85"))
+}
