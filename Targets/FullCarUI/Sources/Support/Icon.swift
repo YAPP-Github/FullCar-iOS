135be00a6a4cf094ae7e94e60fe00a8f89b1ac15
SunnnySong <tjsghktjswls@naver.com>
2024-01-02 05:53:47 +0900

    feat: 이미지를 관리하는 Icon 타입 생성

---
+++ Targets/FullCarUI/Sources/Support/Icon.swift
@@ -0,0 +1,56 @@
+//
+//  Icon.Symbol.swift
+//  FullCarUI
+//
+//  Created by Sunny on 1/1/24.
+//  Copyright © 2024 FullCar Corp. All rights reserved.
+//
+
+import SwiftUI
+
+public struct Icon { 
+    public static func image(type: Icon.Symbol) -> Image? {
+        return type.image
+    }
+}
+
+extension Icon {
+    public enum Symbol {
+        case female
+        case male
+        case quite
+        case talk
+        case check
+
+        public var image: Image? {
+            if let systemName = systemName {
+                return Image(systemName: systemName)
+            } else if let name = name {
+                return Image(name)
+            } else {
+                return nil
+            }
+        }
+
+        private var name: String? {
+            switch self {
+                // 추후 이미지 추가 예정
+            case .female: return "square.and.arrow.up.circle"
+            case .male: return "square.and.arrow.up.circle"
+            case .quite: return "square.and.arrow.up.circle"
+            case .talk: return "square.and.arrow.up.circle"
+            default: return nil
+            }
+        }
+
+        private var systemName: String? {
+            switch self {
+            case .female: return "person.fill"
+            case .male: return "person.fill"
+            case .quite: return "square.and.arrow.up.circle"
+            case .talk: return "square.and.arrow.up.circle"
+            case .check: return "checkmark.circle.fill"
+            }
+        }
+    }
+}
