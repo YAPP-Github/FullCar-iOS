0c063c547401c238b5069c4ae449790bed921faa
SunnnySong <tjsghktjswls@naver.com>
2024-01-05 12:33:48 +0900

    fix: 변경된 font 적용

--- Targets/FullCarUI/Sources/DesignSystem/Font/Pretendard.swift
+++ Targets/FullCarUI/Sources/DesignSystem/Font/Pretendard.swift
@@ -10,59 +10,56 @@ import SwiftUI
 
 public enum Pretendard {
     public enum Style {
-        /// 22, bold
+        /// 22, bold, 34
         case heading1
-        /// 18, bold
+        /// 18, bold, 24
         case heading2
-        /// 18, semibold
-        case heading3
 
-        /// 17, bold
+        /// 17, bold, 20
         case body1
-        /// 17, semibold
+        /// 17, semibold, 20
         case body2
-        /// 16, bold
+        /// 16, bold, 20
         case body3
-        /// 16, semibold
+        /// 16, semibold, 22
         case body4
-        /// 16, regular
+        /// 16, semibold, 19
         case body5
-        /// 15, bold
+        /// 16, regular, 24
         case body6
-        /// 15, medium
+        /// 16, medium, 20
         case body7
 
-        /// 14, semibold
+        /// 14, semibold, 20
         case caption1
-        /// 14, medium
+        /// 14, medium, 20
         case caption2
-        /// 13, bold
+        /// 13, bold, 17
         case caption3
-        /// 13, medium
+        /// 13, semibold, 20
         case caption4
-        /// 12, bold
+        /// 12, bold, 14
         case caption5
-        /// 12, semibold
+        /// 12, semibold, 14
         case caption6
 
         var weight: FontConvertible {
             switch self {
             case .heading1: return Pretendard.bold
             case .heading2: return Pretendard.bold
-            case .heading3: return Pretendard.semiBold
 
             case .body1: return Pretendard.bold
             case .body2: return Pretendard.semiBold
             case .body3: return Pretendard.bold
             case .body4: return Pretendard.semiBold
-            case .body5: return Pretendard.regular
-            case .body6: return Pretendard.bold
+            case .body5: return Pretendard.semiBold
+            case .body6: return Pretendard.regular
             case .body7: return Pretendard.medium
 
             case .caption1: return Pretendard.semiBold
             case .caption2: return Pretendard.medium
             case .caption3: return Pretendard.bold
-            case .caption4: return Pretendard.medium
+            case .caption4: return Pretendard.semiBold
             case .caption5: return Pretendard.bold
             case .caption6: return Pretendard.semiBold
             }
@@ -72,15 +69,14 @@ public enum Pretendard {
             switch self {
             case .heading1: return 22
             case .heading2: return 18
-            case .heading3: return 18
 
             case .body1: return 17
             case .body2: return 17
             case .body3: return 16
             case .body4: return 16
             case .body5: return 16
-            case .body6: return 15
-            case .body7: return 15
+            case .body6: return 16
+            case .body7: return 16
 
             case .caption1: return 14
             case .caption2: return 14
@@ -92,7 +88,23 @@ public enum Pretendard {
         }
 
         var lineHeightSize: CGFloat {
-            return self.size + 12
+            switch self {
+            case .heading1: return 34
+            case .heading2: return 24
+            case .body1: return 20
+            case .body2: return 20
+            case .body3: return 20
+            case .body4: return 22
+            case .body5: return 19
+            case .body6: return 24
+            case .body7: return 20
+            case .caption1: return 20
+            case .caption2: return 20
+            case .caption3: return 17
+            case .caption4: return 20
+            case .caption5: return 14
+            case .caption6: return 14
+            }
         }
     }
 }
