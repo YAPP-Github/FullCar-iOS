11cfbd439d92d713e5fe1d678ffbab406132079a
Tabber <tabber@jejememe.com>
2024-01-06 14:24:01 +0900

    feat: 파이어베이스 추가

--- Targets/FullCar/Sources/FullCarApp.swift
+++ Targets/FullCar/Sources/FullCarApp.swift
@@ -1,8 +1,14 @@
 import SwiftUI
 import FullCarUI
+import Firebase
 
 @main
 struct FullCarApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             RootView(viewModel: .init())
