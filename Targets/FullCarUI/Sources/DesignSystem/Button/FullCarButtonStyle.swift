853e75aeb97ebd3d209318007ae1b1b60fa19160
SunnnySong <tjsghktjswls@naver.com>
2024-01-07 03:01:21 +0900

    feat: 버튼 커스텀 추가

--- Targets/FullCarUI/Sources/DesignSystem/Button/FullCarButtonStyle.swift
+++ Targets/FullCarUI/Sources/DesignSystem/Button/FullCarButtonStyle.swift
@@ -10,44 +10,109 @@ import SwiftUI
 
 public struct FullCarButtonStyle: ButtonStyle {
     @Environment(\.isEnabled) private var isEnabled
-    
+
+    private let font: Pretendard.Style
+    private let verticalPadding: CGFloat
+    private let horizontalPadding: CGFloat
+    private let cornerRadius: CGFloat
+    private let colorStyle: ColorStyle.Palette
+
     public func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .font(pretendard: .body1)
-            .padding(.vertical, Constants.verticalPadding)
-            .frame(maxWidth: .infinity)
-            .foregroundStyle(Colors.text)
-            .background(isEnabled ? Colors.background : Colors.disableBackground)
-            .cornerRadius(radius: Constants.radius, corners: .allCorners)
-    }
-}
-
-extension FullCarButtonStyle {
-    enum Constants {
-        static let verticalPadding: CGFloat = 17
-        static let radius: CGFloat = 8
+            .font(pretendard: font)
+            .padding(.horizontal, horizontalPadding)
+            .padding(.vertical, verticalPadding)
+            .foregroundStyle(foregroundColor)
+            .background(backgroundColor)
+            .cornerRadius(radius: cornerRadius, corners: .allCorners)
     }
 
-    enum Colors {
-        static let text: Color = .white
-        static let background: Color = .fullCar_primary
-        static let disableBackground: Color = .gray30
-    }
-}
-
-struct FullCarButtonStylePreviews: PreviewProvider {
-    static var previews: some View {
-        VStack {
-            Button(action: {}, label: {
-                Text("다음")
-            })
-            .buttonStyle(.fullCar)
-
-            Button(action: {}, label: {
-                Text("다음")
-            })
-            .buttonStyle(.fullCar)
-            .disabled(true)
+    private var foregroundColor: Color {
+        if colorStyle == ColorStyle.Palette.primary_white {
+            switch isEnabled {
+            case true: return ColorStyle.palette(.primary_white).light
+            case false: return ColorStyle.palette(.gray30).light
+            }
+        } else {
+            return ColorStyle.palette(colorStyle).dark
         }
+    }
+
+    private var backgroundColor: Color {
+        if colorStyle == ColorStyle.Palette.primary_white {
+            switch isEnabled {
+            case true: return ColorStyle.palette(.primary_white).dark
+            case false: return ColorStyle.palette(.gray30).dark
+            }
+        } else {
+            return ColorStyle.palette(colorStyle).light
+        }
+    }
+}
+
+public extension FullCarButtonStyle {
+    init(
+        font: Pretendard.Style,
+        horizontalPadding: CGFloat,
+        verticalPadding: CGFloat,
+        radius: CGFloat,
+        colorStyle: ColorStyle.Palette
+    ) {
+        self.font = font
+        self.horizontalPadding = horizontalPadding
+        self.verticalPadding = verticalPadding
+        self.cornerRadius = radius
+        self.colorStyle = colorStyle
+    }
+}
+
+#Preview {
+    VStack(spacing: 20) {
+        Button(action: {}, label: {
+            Text("다음")
+                .frame(maxWidth: .infinity)
+        })
+        .buttonStyle(.fullCar(style: .primary_white))
+
+        Button(action: {}, label: {
+            Text("다음")
+                .frame(maxWidth: .infinity)
+        })
+        .buttonStyle(.fullCar(style: .primary_white))
+        .disabled(true)
+
+        Button(action: {}, label: {
+            Text("인증메일 발송")
+                .frame(maxWidth: .infinity)
+        })
+        .buttonStyle(.fullCar(style: .primary_white))
+        .disabled(true)
+
+        Button(action: {}, label: {
+            Text("요청거절")
+        })
+        .buttonStyle(.fullCar(
+            horizontalPadding: 22,
+            style: .gray60
+        ))
+        .disabled(true)
+
+        Button(action: {}, label: {
+            Text("요청승인")
+        })
+        .buttonStyle(.fullCar(
+            horizontalPadding: 70,
+            style: .primary_white
+        ))
+
+        Button(action: {}, label: {
+            Text("검색")
+        })
+        .buttonStyle(.fullCar(
+            font: .body4,
+            horizontalPadding: 14,
+            verticalPadding: 15,
+            style: .primary_secondary
+        ))
     }
 }
--- Targets/FullCarUI/Sources/Extensions/ButtonStyle+.swift
+++ Targets/FullCarUI/Sources/Extensions/ButtonStyle+.swift
@@ -9,10 +9,21 @@
 import SwiftUI
 
 extension ButtonStyle where Self == FullCarButtonStyle {
-
-    /// 계층 구조가 가장 높은 행동을 유도하는 Button에 한정해 사용합니다.
-    public static var fullCar: FullCarButtonStyle {
-        FullCarButtonStyle()
+    /// 폰트, 색상, padding 등을 커스텀 할 수 있는 버튼입니다. 버튼의 색상은 ColorStyle을 사용합니다.
+    public static func fullCar(
+        font: Pretendard.Style = .body1,
+        horizontalPadding: CGFloat = 0,
+        verticalPadding: CGFloat = 17,
+        radius: CGFloat = 8,
+        style: ColorStyle.Palette
+    ) -> FullCarButtonStyle {
+        FullCarButtonStyle(
+            font: font,
+            horizontalPadding: horizontalPadding,
+            verticalPadding: verticalPadding,
+            radius: radius,
+            colorStyle: style
+        )
     }
 
     /// Basic Filter Chips는 Selected/Unselected에 따라 Default, Active 상태로 분류됩니다. Selected는 Active된 상태로 간주됩니다.
