5a1ad4936a60877f246d2a356bea7dfe13afcd75
SangJin Han <s202188@naver.com>
2024-01-07 16:27:02 +0900

    HomeCardView 구현

--- Targets/FullCar/Sources/Home/HomeCard.swift
+++ Targets/FullCar/Sources/Home/HomeCard.swift
@@ -7,27 +7,64 @@
 //
 
 import SwiftUI
+import FullCarUI
 
+@MainActor
 struct HomeCard: View {
     
     let carPull: Home.Model.TempCarPull
     
     var body: some View {
+        bodyView
+            .padding(.vertical, 18)
+            .padding(.horizontal, 20)
+            .background(Color.white)
+    }
+    
+    private var bodyView: some View {
         VStack(alignment: .leading, spacing: .zero) { 
             HStack(spacing: .zero) {
                 Text(carPull.companyName)
+                    .font(pretendard: .caption1)
+                    .foregroundStyle(Color.fullCar_primary)
                 Spacer()
-                Text(carPull.state.description)
+                Badge(
+                    title: "모집중",
+                    configurable: .standard,
+                    style: .palette(.primary_secondary)
+                )
             }
+            .padding(.bottom, 14)
+            
             Text(carPull.title)
+                .font(pretendard: .body1)
+                .padding(.bottom, 12)
+            
             Text(carPull.description)
-            // chips
+                .font(pretendard: .body6)
+                .padding(.bottom, 10)
+            
+            HStack(spacing: 6) {
+                Badge(.female)
+                Badge(.quiet)
+            }
+            
             Divider()
+                .padding(.vertical, 18)
+            
             HStack(spacing: .zero) {
                 Text("희망비용")
+                    .font(pretendard: .caption1)
+                    .foregroundStyle(Color.gray50)
+                    .padding(.trailing, 8)
+                
                 Text("48,000원")
+                    .font(pretendard: .caption1)
+                    .foregroundStyle(Color.black80)
                 Spacer()
                 Text("12월 28일")
+                    .font(pretendard: .caption2)
+                    .foregroundStyle(Color.gray40)
             }
         }
     }
