7b8db0c982fab410cb13da46f2692e9da7e8cc83
SangJin Han <s202188@naver.com>
2024-01-07 16:36:21 +0900

    Home View 일단 구현

--- Targets/FullCar/Sources/Home/HomeView.swift
+++ Targets/FullCar/Sources/Home/HomeView.swift
@@ -7,13 +7,36 @@
 //
 
 import SwiftUI
+import FullCarUI
 
 @MainActor
 struct HomeView: View {
     let viewModel: HomeViewModel
     var body: some View {
-        bodyView
-            .onFirstTask { await viewModel.onFirstTask() }
+        VStack(spacing: .zero) {
+            headerView
+            bodyView
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.gray10)
+        .onFirstTask { await viewModel.onFirstTask() }
+    }
+    private var headerView: some View {
+        HStack(spacing: .zero) {
+            Text("야놀자")
+                .font(pretendard: .heading2)
+                .foregroundStyle(Color.black80)
+                .debug()
+            Spacer()
+            Image("FCHomeTopTrailingImage", bundle: .main)
+                .debug()
+        }
+        .padding(.leading, 20)
+        .padding(.trailing, 15.48)
+        .frame(height: 61)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .debug(color: .red)
     }
     @ViewBuilder
     private var bodyView: some View {
@@ -26,6 +49,7 @@ struct HomeView: View {
     private func carPullList(_ list: [Home.Model.TempCarPull]) -> some View {
         ForEach(list) { carpull in
             HomeCard(carPull: carpull)
+                .padding(.bottom, 8)
         }
         .refreshable(action: viewModel.refreshable)
     }
@@ -35,5 +59,7 @@ struct HomeView: View {
 }
 
 #Preview {
-    HomeView(viewModel: .init())
+    NavigationStack {
+        HomeView(viewModel: .init())
+    }
 }
