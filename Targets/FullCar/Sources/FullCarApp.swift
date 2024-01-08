0a5b34da994f7cc9d0a5e24815fbab935a2154bb
Tabber <tabber@jejememe.com>
2024-01-08 21:30:43 +0900

    fix: entitlements 에러 임시 수정, firebase 세팅 코드 이동

--- Targets/FullCar/Sources/FullCarApp.swift
+++ Targets/FullCar/Sources/FullCarApp.swift
@@ -1,13 +1,8 @@
 import SwiftUI
 import FullCarUI
-import Firebase
 
 @main
 struct FullCarApp: App {
-    
-    init() {
-        FirebaseApp.configure()
-    }
     
     var body: some Scene {
         WindowGroup {
