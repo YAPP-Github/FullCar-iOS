46447d6a8115da3648487d53e6eeef996801bd00
SunnnySong <tjsghktjswls@naver.com>
2024-01-17 04:35:03 +0900

    feat: SocialType -> FullCarKit에 선언 및 관련 extension 구현

---
+++ Targets/FullCarKit/Sources/Account/Model/SocialType.swift
@@ -0,0 +1,14 @@
+//
+//  SocialType.swift
+//  FullCarKit
+//
+//  Created by Sunny on 1/15/24.
+//  Copyright © 2024 FullCar Corp. All rights reserved.
+//
+
+import Foundation
+
+public enum SocialType: String {
+    case kakao = "KAKAO"
+    case apple = "APPLE"
+}
---
+++ Targets/FullCarUI/Sources/DesignSystem/Login/SocialType+.swift
@@ -0,0 +1,41 @@
+//
+//  SocialType+.swift
+//  FullCarUI
+//
+//  Created by Sunny on 1/15/24.
+//  Copyright © 2024 FullCar Corp. All rights reserved.
+//
+
+import SwiftUI
+import FullCarKit
+
+public extension SocialType {
+    var title: String {
+        switch self {
+        case .kakao: return "카카오 로그인"
+        case .apple: return "Apple로 로그인"
+        }
+    }
+
+    var icon: Image {
+        switch self {
+        case .kakao: return Image(icon: .kakaoLogo)
+        case .apple: return Image(icon: .appleLogo)
+        }
+    }
+
+    var backgroundColor: Color {
+        switch self {
+        case .kakao: return Color(cgColor: UIColor(hex: "FEE500").cgColor)
+        case .apple: return .black
+        }
+    }
+
+    var fontColor: Color {
+        switch self {
+        case .kakao: return Color(cgColor: UIColor(hex: "392020").cgColor)
+        case .apple: return .white
+        }
+    }
+}
+
