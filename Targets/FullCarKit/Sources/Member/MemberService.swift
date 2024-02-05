2b86684dafdb866e2dc5a404d6ba3542cbe9c2b0
SunnnySong <tjsghktjswls@naver.com>
2024-02-05 13:59:25 +0900

    feat: MemberService 타입 생성 및 구현

---
+++ Targets/FullCarKit/Sources/Member/MemberService.swift
@@ -0,0 +1,34 @@
+//
+//  MemberService.swift
+//  FullCarKit
+//
+//  Created by Sunny on 2/5/24.
+//  Copyright © 2024 FullCar Corp. All rights reserved.
+//
+
+import Foundation
+import Dependencies
+
+public struct MemberService {
+    public var locationSearch: (_ location: String, _ key: String) async throws -> Void
+}
+
+extension MemberService: DependencyKey {
+    public static var liveValue: MemberService {
+        @Dependency(\.memberAPI) var api
+
+        return MemberService(
+            locationSearch: { location,key in
+                let coordinate = try await api.locationSearch(location, key)
+                print(coordinate)
+            }
+        )
+    }
+}
+
+extension DependencyValues {
+    public var memberService: MemberService {
+        get { self[MemberService.self] }
+        set { self[MemberService.self] = newValue }
+    }
+}
