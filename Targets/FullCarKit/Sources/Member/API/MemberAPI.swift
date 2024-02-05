678a01de32eac750d4b1ccd3c65a4c84273080b2
SunnnySong <tjsghktjswls@naver.com>
2024-02-05 13:58:07 +0900

    feat: MemberAPI, CompanyCoordinate 생성 및 구현

---
+++ Targets/FullCarKit/Sources/Member/API/MemberAPI.swift
@@ -0,0 +1,46 @@
+//
+//  MemberAPI.swift
+//  FullCarKit
+//
+//  Created by Sunny on 2/5/24.
+//  Copyright © 2024 FullCar Corp. All rights reserved.
+//
+
+import SwiftUI
+import Dependencies
+
+struct MemberAPI {
+    var locationSearch: (_ location: String, _ key: String) async throws -> CompanyCoordinate
+}
+
+extension MemberAPI: DependencyKey {
+    static var liveValue: MemberAPI {
+        return MemberAPI(
+            locationSearch: { location, key in
+                let coordinate: CompanyCoordinate = try await NetworkClient.main.request(
+                    endpoint: Endpoint.Member.locationSearch(location, key: key)
+                ).response()
+                return coordinate
+            }
+        )
+    }
+
+    #if DEBUG
+    static let testValue: MemberAPI = .init(
+        locationSearch: { _, _ in
+            return .init(
+                name: "네이버",
+                latitude: 127.10520633434606,
+                longitude: 37.3588600621634
+            )
+        }
+    )
+    #endif
+}
+
+extension DependencyValues {
+    var memberAPI: MemberAPI {
+        get { self[MemberAPI.self] }
+        set { self[MemberAPI.self] = newValue }
+    }
+}
---
+++ Targets/FullCarKit/Sources/Member/Model/CompanyCoordinate.swift
@@ -0,0 +1,15 @@
+//
+//  CompanyCoordinate.swift
+//  FullCarKit
+//
+//  Created by Sunny on 2/5/24.
+//  Copyright © 2024 FullCar Corp. All rights reserved.
+//
+
+import Foundation
+
+struct CompanyCoordinate: Decodable {
+    let name: String
+    let latitude: Double
+    let longitude: Double
+}
