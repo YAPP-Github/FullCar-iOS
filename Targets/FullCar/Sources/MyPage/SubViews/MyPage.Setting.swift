5ccfb39e02f9b73f99dbd8b6c509a57d5bfc6284
SunnnySong <tjsghktjswls@naver.com>
2024-02-20 01:46:14 +0900

    feat: 탈퇴, 로그아웃 실패 얼럿 추가

--- Targets/FullCar/Sources/MyPage/SubViews/MyPage.Setting.swift
+++ Targets/FullCar/Sources/MyPage/SubViews/MyPage.Setting.swift
@@ -15,11 +15,11 @@ extension MyPage.Setting {
         @Environment(\.dismiss) var dismiss
         @Bindable var viewModel: MyPage.ViewModel
 
-        @State private var isShowingLogoutDialog = false
-        @State private var isShowingLeaveDialog = false
+        @State private var isShowLogoutDialog = false
+        @State private var isShowLeaveDialog = false
 
-        @State private var isShowingLogoutAlert = false
-        @State private var isShowingLeaveAlert = false
+        @State private var isShowLogoutAlert = false
+        @State private var isShowLeaveAlert = false
 
         var body: some View {
             bodyView
@@ -44,22 +44,22 @@ extension MyPage.Setting {
         }
 
         private var logoutView: some View {
-            buttonItem(text: "로그아웃", action: { isShowingLogoutDialog = true })
+            buttonItem(text: "로그아웃", action: { isShowLogoutDialog = true })
                 .confirmationDialog(
                     "",
-                    isPresented: $isShowingLogoutDialog,
+                    isPresented: $isShowLogoutDialog,
                     titleVisibility: .hidden
                 ) {
                     Button("로그아웃", role: .destructive) {
-                        isShowingLogoutAlert = true
+                        isShowLogoutAlert = true
                     }
                     Button("닫기", role: .cancel) {
-                        isShowingLogoutDialog = false
+                        isShowLogoutDialog = false
                     }
                 }
                 .alert(
                     "로그아웃하시겠어요?",
-                    isPresented: $isShowingLogoutAlert
+                    isPresented: $isShowLogoutAlert
                 ) {
                     Button("로그아웃", role: .destructive) {
                         Task {
@@ -67,28 +67,28 @@ extension MyPage.Setting {
                         }
                     }
                     Button("취소", role: .cancel) {
-                        isShowingLogoutAlert = false
+                        isShowLogoutAlert = false
                     }
                 }
         }
 
         private var leaveView: some View {
-            buttonItem(text: "탈퇴", action: { isShowingLeaveDialog = true })
+            buttonItem(text: "탈퇴", action: { isShowLeaveDialog = true })
                 .confirmationDialog(
                     "",
-                    isPresented: $isShowingLeaveDialog,
+                    isPresented: $isShowLeaveDialog,
                     titleVisibility: .hidden
                 ) {
                     Button("탈퇴", role: .destructive) {
-                        isShowingLeaveAlert = true
+                        isShowLeaveAlert = true
                     }
                     Button("닫기", role: .cancel) {
-                        isShowingLeaveDialog = false
+                        isShowLeaveDialog = false
                     }
                 }
                 .alert(
                     "정말 탈퇴하시겠어요?",
-                    isPresented: $isShowingLeaveAlert,
+                    isPresented: $isShowLeaveAlert,
                     actions: {
                         Button("탈퇴하기", role: .destructive) {
                             Task {
@@ -96,14 +96,14 @@ extension MyPage.Setting {
                             }
                         }
                         Button("취소", role: .cancel) {
-                            isShowingLeaveAlert = false
+                            isShowLeaveAlert = false
                         }
                     },
                     message: { Text("탈퇴 시 재가입이 어려울 수 있으며,\n작성한 모든 게시글에 대한 권한이 소멸됩니다.") }
                 )
                 .alert(
                     "정상적으로 탈퇴처리 되었습니다.",
-                    isPresented: $viewModel.isShowingLeaveCompletionAlert,
+                    isPresented: $viewModel.isShowLeaveCompletionAlert,
                     actions: {
                         Button("확인", role: .cancel) {
                             viewModel.completeLeave()
